d413386f377ec91e57a166ae748374ab10a05591
Joao <facedojm@gmail.com>
2025-10-18 18:46:34 -0300

    feat: pipeline para ingestão e atualização (11)

--- src/analytics/frequencia_valor.py
+++ src/analytics/frequencia_valor.py
@@ -1,10 +1,11 @@
 # %%
 
 import pandas as pd
-import numpy as np
 import sqlalchemy 
+
 import matplotlib.pyplot as plt
 import seaborn as sns
+
 from sklearn import cluster
 from sklearn import preprocessing
 
